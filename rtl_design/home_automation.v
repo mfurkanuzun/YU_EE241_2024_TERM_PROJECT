2ab304a99b131b8928f3d0622c1833bac9452df9
mhfuzun <mhfurkan.uzun@gmail.com>
2024-11-05 21:17:37 +0300

    fix (module)

--- rtl_design/home_automation.v
+++ rtl_design/home_automation.v
@@ -22,8 +22,11 @@
 
 module home_automation
     (
+        input[`TEMPERATURE_SENSOR_DATA_WIDTH-1:0] temparature_i,
         output AC_heat_o, AC_cool_o
     );
+    
+    reg [1:0] ac_working_mode_r;
     
     wire TEMPCONTROLLER_ac_heat_w;
     wire TEMPCONTROLLER_ac_cool_w;
@@ -36,6 +39,16 @@ module home_automation
     */
     assign AC_heat_o = ~NOWASTE_ac_energy_w & TEMPCONTROLLER_ac_heat_w;
     assign AC_cool_o = ~NOWASTE_ac_energy_w & TEMPCONTROLLER_ac_cool_w;
+    
+    // AC temp controller
+    ac_temp_controller AC_TEMP_CONTROLLER
+    (
+        .ac_working_mode_i(ac_working_mode_r),
+        .temparature_i(temparature_i),
+        
+        .heater_mode_active_o(TEMPCONTROLLER_ac_heat_w),
+        .cooler_mode_active_o(TEMPCONTROLLER_ac_cool_w)
+    );
     
 endmodule
 
