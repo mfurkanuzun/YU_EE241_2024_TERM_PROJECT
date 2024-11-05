42e675658aa66542786ae3f9dbc316e513f1cb98
mhfuzun <mhfurkan.uzun@gmail.com>
2024-11-05 20:14:15 +0300

    feat(inc folder): design constants file

---
+++ rtl_design/inc/design_constant.vh
@@ -0,0 +1,11 @@
+// desgin constant
+
+// topmodule
+`define PERSON_COUNTER_DATA_WIDTH                   16
+
+
+`define HOME_WINDOW_COUNT                           8
+`define HOME_DOOR_COUNT                             4
+
+`define TEMPERATURE_SENSOR_DATA_WIDTH               8
+
---
+++ rtl_design/security_controller.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.11.2024 20:10:02
+// Design Name: 
+// Module Name: security_controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+`include "design_constant.vh"
+
+module security_controller
+    (
+        input [`PERSON_COUNTER_DATA_WIDTH-1:0] person_count_i
+    );
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
