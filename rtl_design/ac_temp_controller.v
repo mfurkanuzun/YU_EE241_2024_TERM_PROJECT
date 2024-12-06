9b4d936ee81cfec8c9bca145e250dc0ade0920ad
mhfuzun <166492157+mhfuzun@users.noreply.github.com>
2024-12-06 10:19:42 +0300

    Update ac_temp_controller.v

--- rtl_design/ac_temp_controller.v
+++ rtl_design/ac_temp_controller.v
@@ -17,3 +17,29 @@ module ac_temp_controller(
     
 
 endmodule
+
+
+module ac_temp_controller_bhv(
+    input [1:0] ac_working_mode_i,
+    input [`TEMPERATURE_SENSOR_DATA_WIDTH-1:0] temparature_i,
+    output reg heater_mode_active_o,
+    output reg cooler_mode_active_o
+);
+
+    always @(*) begin
+        // Soğutucu modu kontrolü
+        if (temparature_i > `TEMPERATURE_SENSOR_MAX_TEMP && ac_working_mode_i[`AC_COOL_MODE_BIT]) begin
+            cooler_mode_active_o = 1'b1;
+        end else begin
+            cooler_mode_active_o = 1'b0;
+        end
+
+        // Isıtıcı modu kontrolü
+        if (temparature_i < `TEMPERATURE_SENSOR_MIN_TEMP && ac_working_mode_i[`AC_HEAT_MODE_BIT]) begin
+            heater_mode_active_o = 1'b1;
+        end else begin
+            heater_mode_active_o = 1'b0;
+        end
+    end
+
+endmodule
