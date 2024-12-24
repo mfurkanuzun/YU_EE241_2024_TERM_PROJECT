cfceb4cd2dff6cabede29e015e87d1bbaf496a54
Muhammetcemil <muhammetcemil.ogutveren@std.yeditepe.edu.tr>
2024-12-24 17:37:04 +0300

    2

---
+++ ac_control_unit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.12.2024 17:35:54
+// Design Name: 
+// Module Name: ac_control_unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ac_control_unit(
+
+    );
+endmodule
